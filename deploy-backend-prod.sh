aa2dd2bfbb542fa6eca2ec0d72f173b5a58ec4e9
v1ctor <av1ctortv@gmail.com>
2022-06-17 10:02:11 -0300

    fix: production deployment

--- deploy-backend-prod.sh
+++ deploy-backend-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-MMT_CANISTER_ID=lyp45-xyaaa-aaaah-abldq-cai
+MMT_CANISTER_ID=tbqmo-6aaaa-aaaan-qajoq-cai
 LEDGER_CANISTER_ID=ryjl3-tyaaa-aaaaa-aaaba-cai
-eval dfx deploy metamob --network ic --with-cycles 100000000000 --argument="'(\"$LEDGER_CANISTER_ID\", \"$MMT_CANISTER_ID\")'"
+eval dfx deploy metamob --network ic --with-cycles 1000000000000 --argument="'(\"$LEDGER_CANISTER_ID\", \"$MMT_CANISTER_ID\")'"
---
+++ deploy-frontend-prod.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+eval dfx deploy --network ic --with-cycles 1000000000000 metamob_assets
