384750da2028955d2487f3201d96f0f401765a21
v1ctor <av1ctortv@gmail.com>
2022-06-28 14:00:17 -0300

    fix: moved scripts to their own folder

--- deploy-ii.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl -sSL https://github.com/dfinity/internet-identity/releases/latest/download/internet_identity_dev.wasm -o internet_identity.wasm
-dfx deploy --no-wallet --argument '(null)' internet_identity
--- scripts/deploy-backend-prod.sh
+++ scripts/deploy-backend-prod.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-MMT_CANISTER_ID=tbqmo-6aaaa-aaaan-qajoq-cai
-LEDGER_CANISTER_ID=ryjl3-tyaaa-aaaaa-aaaba-cai
+MMT_CANISTER_ID="$(dfx canister --network ic id mmt)"
+LEDGER_CANISTER_ID="$(dfx canister --network ic id ledger)"
 eval dfx deploy metamob --network ic --with-cycles 1000000000000 --argument="'(\"$LEDGER_CANISTER_ID\", \"$MMT_CANISTER_ID\")'"
