2ae96002ec5e229a1487917cf781e9432d8f4b27
v1ctor <av1ctortv@gmail.com>
2022-11-11 12:37:56 -0300

    fix: layout

--- src/metamob_assets/src/views/places/place/Children.tsx
+++ src/metamob_assets/src/views/places/place/Children.tsx
@@ -70,7 +70,7 @@ const Children = (props: Props) => {
                     </Fragment>
                 )
             :
-                Array.from([0,1,2,3,4,5,6,7,8,9]).map(index => 
+                Array.from([1,2,3]).map(index => 
                     <Skeleton key={index} className="place-child" />
                 )
             }
