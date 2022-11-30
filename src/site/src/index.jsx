f759576c35d29a2b440f3f8931853259c16fef48
v1ctor <av1ctortv@gmail.com>
2022-11-30 09:53:37 -0300

    fix: import

--- src/site/src/index.jsx
+++ src/site/src/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from 'react-dom/client';
 import {App} from './App';
 
-import 'react-loading-skeleton/dist/skeleton.css';
+import '../../../node_modules/react-loading-skeleton/dist/skeleton.css';
 import '../sass/mystyles.scss';
 
 const container = document.getElementById('app');
