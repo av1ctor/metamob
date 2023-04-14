06d15a4a3fea8e3a2e06d52fef6c3ae0de4bf3de
av1ctor <av1ctortv@gmail.com>
2023-04-14 16:42:37 -0300

    fix: set the identity

--- scripts/deploy-filestore-prod.sh
+++ scripts/deploy-filestore-prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 METAMOB_CANISTER_ID="$(dfx canister id metamob --network ic)"
-eval dfx deploy filestore --network ic --with-cycles 1000000000000 --argument="'(vec {principal \"$METAMOB_CANISTER_ID\"}, vec {\"localhost:8080\"; \"127.0.0.1:8080\"})'"
+eval dfx deploy filestore --network ic --identity deployer --with-cycles 1000000000000 --argument="'(vec {principal \"$METAMOB_CANISTER_ID\"}, vec {\"localhost:8080\"; \"127.0.0.1:8080\"})'"
--- scripts/deploy-logger-prod.sh
+++ scripts/deploy-logger-prod.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 METAMOB_CANISTER_ID="$(dfx canister id metamob --network ic)"
-eval dfx deploy logger --network ic --with-cycles 1000000000000 --argument="'(vec {principal \"$METAMOB_CANISTER_ID\"})'"
+eval dfx deploy logger --network ic --identity deployer --with-cycles 1000000000000 --argument="'(vec {principal \"$METAMOB_CANISTER_ID\"})'"
